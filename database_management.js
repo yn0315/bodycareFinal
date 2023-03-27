15bf85551ec555031cc7826240b6757e2dad270b
yn0315 <yunu0315@naver.com>
2023-03-27 20:39:01 +0900

    [2023.03.27]

--- database_management.js
+++ database_management.js
@@ -18,7 +18,7 @@ class DbManager
     constructor()
     {
         this.conn ={
-            host : 'localhost',
+            host : '127.0.0.1',
             port : '3306',
             user : 'root',
             password : '0000',
